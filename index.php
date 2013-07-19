ef95162d0b4722205fc90472707f17088fc1653a
Rainer Karcher <rainer.tobias.karcher@gmail.com>
2013-07-19 18:54:22 -0500

    Added image carousel

--- index.php
+++ index.php
@@ -1,11 +1,61 @@
 <?php include('header.php') ?>
-    <div class="container">
-      <h1>About</h1>
-      <p>I am a Linux systems administrator and full-stack web developer based in Tuscaloosa, AL, and I create professional and responsive websites and web applications using the latest in W3C and SEO web standards.</p>
-      <p>My current weapons of choice in website development are WordPress and Bootstrap (my development capabilities and resume can be found in <a href="http://zerply.com/quizas/work">greater detail here</a>) and I'm currently learning Ruby and dusting off Python to bolster my skills as an application developer.</p>
-      <p>When I'm not writing code or pushing pixels, I enjoy playing drums, trying out new Linux distros, studying Chinese literature, and reading up on the latest research in neuroscience and biomedical engineering<!--, and constructing multi-story rabbit condominiums for my pet rabbit, Maddie.--></p>
-      <h5><a href="http://rainerkarcher.com/bootstrap/img/uploads/Resume_2013.pdf" target="_blank"><img src="http://rainerkarcher.com/bootstrap/img/uploads/pdf.png">Resume (Print-friendly PDF)</a></h5>
-    </div> <!-- End div.container -->
+<script>
+  $(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 3000
+    });
+  });
+</script>
+<div class="container">
+	<!--  Carousel - consult the Twitter Bootstrap docs at
+      http://twitter.github.com/bootstrap/javascript.html#carousel -->
+<div id="this-carousel-id" class="carousel slide"><!-- class of slide for animation -->
+  <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+  <div class="carousel-inner">
+    <div class="item active"><!-- class of active since it's the first item -->
+      <img src="http://rainerkarcher.com/bootstrap/img/uploads/work/phycolab_1200x480.jpg" alt="" />
+      <div class="carousel-caption">
+	 <h4>Phycology Laboratory</h4>
+	 <p>University of Alabama<br />
+	 <a href="#" target="_blank">http://phycolab.ua.edu</a></p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://rainerkarcher.com/bootstrap/img/uploads/work/theatre_1200x480.jpg" alt="" />
+      <div class="carousel-caption">
+	 <h4>Department of Theatre & Dance</h4>
+	 <p>University of Alabama<br />
+	 <a href="http://www.theatre.ua.edu" target="_blank">http://www.theatre.ua.edu</a></p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://rainerkarcher.com/bootstrap/img/uploads/work/mint_1200x480.jpg" alt="" />
+      <div class="carousel-caption">
+	 <h4>Center for Materials for Information Technology</h4>
+	 <p>University of Alabama<br />
+	 <a href="http://www.mint.ua.edu" target="_blank">http://www.mint.ua.edu</a></p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://rainerkarcher.com/bootstrap/img/uploads/work/etech_dashboard_1200x480.jpg" alt="" />
+      <div class="carousel-caption">
+	 <h4>Office of Educational Technology WordPress Plugin</h4>
+	 <p>University of Alabama<br />
+	 GitHub Project: <a href="http://www.theatre.ua.edu" target="_blank">https://github.com/rtkarcher/etech-wordpress-dashboard-widget</a></p>
+      </div>
+    </div>
+  </div><!-- /.carousel-inner -->
+  <!--  Next and Previous controls below
+        href values must reference the id for this carousel -->
+    <a class="carousel-control left" href="#this-carousel-id" data-slide="prev">&lsaquo;</a>
+    <a class="carousel-control right" href="#this-carousel-id" data-slide="next">&rsaquo;</a>
+</div><!-- /.carousel -->
+</div> <!-- End div.container -->
 
 <br /><br /><br /><br />
     <script src="/bootstrap/js/bootstrap.js"></script>
